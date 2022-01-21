b81b65ec818387c61ab23ed8be440957229c00b1
MatMil33 <94191475+MatMil33@users.noreply.github.com>
2022-01-21 10:59:03 +0100

    Add files via upload

---
+++ ProjetSama/src/test/java/fr/sama/demo/ProjetSamaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sama.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
