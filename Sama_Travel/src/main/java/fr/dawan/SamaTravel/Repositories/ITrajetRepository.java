8921f7d70651e7104e49cf6de15678a8d53a2d62
Sanka <tabarys@outlook.fr>
2022-01-21 14:47:31 +0100

    Interfaces

---
+++ Sama_Travel/src/main/java/fr/dawan/SamaTravel/Repositories/ITrajetRepository.java
@@ -0,0 +1,9 @@
+package fr.dawan.SamaTravel.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.dawan.SamaTravel.entities.Trajet;
+
+public interface ITrajetRepository extends JpaRepository<Trajet, Integer>{
+
+}
