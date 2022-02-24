3b712286b40bda66113b6a2c38af05d74fee0b97
admin stagiaire <admin stagiaire@DESKTOP-RNREJ3V.home>
2022-02-24 18:51:28 +0100

    CrossOrigin

--- Sama_Travel/src/main/java/fr/dawan/SamaTravel/SamaTravelApplication.java
+++ Sama_Travel/src/main/java/fr/dawan/SamaTravel/SamaTravelApplication.java
@@ -2,7 +2,9 @@ package fr.dawan.SamaTravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import fr.dawan.SamaTravel.entities.BeanConfiguration;
 
